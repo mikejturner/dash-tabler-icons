5c1945adb6df1c1283c4c8d42295e83bd278f506
Mike Turner <mike.j.turner@gmail.com>
2022-10-28 13:39:33 +0800

    Initial commit

---
+++ R/internal.R
@@ -0,0 +1,15 @@
+.dashTablerIcons_js_metadata <- function() {
+deps_metadata <- list(`dash_tabler_icons` = structure(list(name = "dash_tabler_icons",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dash_tabler_icons.min.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashTablerIcons",
+all_files = FALSE), class = "html_dependency"),
+`dash_tabler_icons` = structure(list(name = "dash_tabler_icons",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dash_tabler_icons.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashTablerIcons",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
+return(deps_metadata)
+}
---
+++ usage.py
@@ -0,0 +1,22 @@
+import dash_tabler_icons
+from dash import Dash, callback, html, Input, Output
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dash_tabler_icons.DashTablerIcons(
+        id='input',
+        value='my-value',
+        label='my-label'
+    ),
+    html.Div(id='output')
+])
+
+
+@callback(Output('output', 'children'), Input('input', 'value'))
+def display_output(value):
+    return 'You have entered {}'.format(value)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
